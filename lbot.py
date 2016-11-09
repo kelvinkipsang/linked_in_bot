be54bb7a23d079d28c40b5187d9a242ece312103
kelvinkipsang <kelvinkipsang@gmail.com>
2016-11-09 17:52:30 +0300

    creating function to get links

---
+++ lbot.py
@@ -0,0 +1,9 @@
+import argparse, os, time 
+import urlparse, random
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup4
+
+def getPeoplesLinks(page):
+	links[]
+	
