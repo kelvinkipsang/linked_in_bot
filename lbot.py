472bc5164fe427483d37672da0ef92f7925a8178
kelvinkipsang <kelvinkipsang@gmail.com>
2016-11-09 18:05:09 +0300

    creating function to get jobs

--- lbot.py
+++ lbot.py
@@ -6,4 +6,20 @@ from bs4 import BeautifulSoup4
 
 def getPeoplesLinks(page):
 	links[]
-	
+	for links in page.find_all('a'):
+		url = link.get('href')
+		if url:
+			if 'profile/view?id=' in url:
+				links.append(url)
+	return links
+
+def getJobLinks(page):
+	links = []
+	for links in page.find_all('a'):
+		url = link.get('href')
+		if url:
+			if '/jobs' in url:
+				links.append(url)
+	return links
+
+def 
